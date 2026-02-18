afe0a666a472990993ad0f66dfb9a2e81494ef66
santhoshkannan007 <myecommercesyt@gmail.com>
2026-02-18 10:48:34 +0530

    first commit

---
+++ students/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Student
+from .forms import StudentForm
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/list.html', {'students': students})
+
+def student_create(request):
+    form = StudentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('student_list')
+    return render(request, 'students/form.html', {'form': form})
+
+def student_update(request, id):
+    student = Student.objects.get(id=id)
+    form = StudentForm(request.POST or None, instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect('student_list')
+    return render(request, 'students/form.html', {'form': form})
+
+def student_delete(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect('student_list')
